101466458469d257ef42d413cfc7846b09dd5283
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-06-11 22:40:36 +1000

    pydebug script that uses address sanitiser

---
+++ python/pydebug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+make clean
+rm -rf *.so python/pyslow5.cpp build/lib.* build/temp.*
+GCC_ASAN_PRELOAD=$(gcc -print-file-name=libasan.so)
+CFLAGS="-fsanitize=address -fno-omit-frame-pointer" python3 setup.py build
+cp build/lib.*/*.so  ./
+echo $GCC_ASAN_PRELOAD
+LD_PRELOAD=$GCC_ASAN_PRELOAD  python3 < python/example.py
