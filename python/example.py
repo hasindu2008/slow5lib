ae59e4ce3975884f0502bdc36fa3c3ac1a888c65
Psy-Fer <fergo1988@gmail.com>
2021-06-15 17:36:59 +1000

    Extra checks, example made pretty, working base before larger additions

--- python/example.py
+++ python/example.py
@@ -1,8 +1,10 @@
 import pyslow5
 import time
 
-print("test launch")
 
+print("|==============================================|")
+print("|             pyslow5 test launch              |")
+print("|==============================================|")
 
 
 # open file and create object
@@ -10,9 +12,10 @@ start_time = time.time()
 s5p = pyslow5.slow5py('examples/example.slow5','r')
 ttime = round(time.time() - start_time, 4)
 print("slow5 file opened and object created in: {} seconds".format(ttime))
-
+print("==============================================")
 
 # Get data for ONE individual read, random access
+print("get_read check, r1")
 start_time = time.time()
 read = s5p.get_read("r1")
 ttime = round(time.time() - start_time, 4)
@@ -29,8 +32,30 @@ print("len_raw_signal:", read['len_raw_signal'])
 print("signal:", read['signal'][:10])
 print("pylen of signal:", len(read['signal']))
 
+print("==============================================")
+
+# Get data for ANOTHER ONE individual read, random access, check memory
+print("get_read check, r4")
+start_time = time.time()
+read = s5p.get_read("r4")
+ttime = round(time.time() - start_time, 4)
+print("get_read in: {} seconds".format(ttime))
+
+# print all fields
+print("read_id:", read['read_id'])
+print("read_group:", read['read_group'])
+print("digitisation:", read['digitisation'])
+print("offset:", read['offset'])
+print("range:", read['range'])
+print("sampling_rate:", read['sampling_rate'])
+print("len_raw_signal:", read['len_raw_signal'])
+print("signal:", read['signal'][:10])
+print("pylen of signal:", len(read['signal']))
+
+print("==============================================")
 
 # get yield object and iterate through reads sequentially
+print("Yield check, all reads")
 start_time = time.time()
 reads = s5p.yield_reads()
 ttime = round(time.time() - start_time, 4)
@@ -39,15 +64,24 @@ print("yielded readIDs:")
 for read in reads:
     print(read['read_id'])
 
-
+print("==============================================")
 
 # get yield object and iterate through selected reads with random access
-# start_time = time.time()
-# selected_reads = s5p.yield_read_list(read_list)
-# ttime = round(time.time() - start_time, 4)
-# print("yield_read_list in: {} seconds".format(ttime))
+print("Yield check, selected reads")
+read_list = ["r1", "r3", "null_read", "r5"]
+start_time = time.time()
+selected_reads = s5p.yield_read_list(read_list)
+ttime = round(time.time() - start_time, 4)
+print("yield_read_list in: {} seconds".format(ttime))
+print("yielded readIDs:")
+for r, read in zip(read_list,selected_reads):
+    if read is not None:
+        print(r, read['read_id'])
+    else:
+        print(r, "read not found, None returned")
 
 
+print("==============================================")
 
 
 
--- python/pyslow5.pyx
+++ python/pyslow5.pyx
@@ -30,7 +30,8 @@ cdef class slow5py:
         print("Number of read_groups:", self.s5p.header.num_read_groups)
         print("Creating/loading index...", file=sys.stderr)
         ret = slow5_idx_load(self.s5p)
-        print("Index returned: ", ret, file=sys.stderr)
+        if ret != 0:
+            print("slow5_idx_load return not 0:", ret, file=sys.stderr)
 
     def __dealloc__(self):
         if self.rec is not NULL:
@@ -41,12 +42,14 @@ cdef class slow5py:
             pyslow5.slow5_close(self.s5p)
 
 
-    def get_read(self, read_id):
+    def _get_read(self, read_id, pA=False):
         dic = {}
         ID = str.encode(read_id)
         # rec = NULL
         ret = slow5_get(ID, &self.rec, self.s5p)
-        print("get return:", ret, file=sys.stderr)
+        if ret != 0:
+            print("get_read return not 0:", ret, file=sys.stderr)
+            return None
         # for i in range(20):
         #     print(self.rec.raw_signal[i])
         dic['read_id'] = self.rec.read_id
@@ -56,11 +59,21 @@ cdef class slow5py:
         dic['range'] = self.rec.range
         dic['sampling_rate'] = self.rec.sampling_rate
         dic['len_raw_signal'] = self.rec.len_raw_signal
-        dic['signal'] = []
-        for i in range(self.rec.len_raw_signal):
-            dic['signal'].append(self.rec.raw_signal[i])
-        
+        dic['signal'] = [self.rec.raw_signal[i] for i in range(self.rec.len_raw_signal)]
+        # This could be handled by numpy.fromiter() or similar
+        # Probably MUCH faster
+        # https://stackoverflow.com/questions/7543675/how-to-convert-pointer-to-c-array-to-python-array
+        # for i in range(self.rec.len_raw_signal):
+        #     dic['signal'].append(self.rec.raw_signal[i])
+        # if pA=True, convert signal to pA
+        if pA:
+            # call some conversion function
+            pass
+
         return dic
+
+    def get_read(self, read_id, pA=False):
+        return self._get_read(read_id, pA=pA)
 
 
     def yield_reads(self, pA=False):
@@ -73,7 +86,10 @@ cdef class slow5py:
             ret = slow5_get_next(&self.read, self.s5p)
             print("TEST READID", self.read.read_id)
             print("slow5_get_next return:", ret, file=sys.stderr)
+            # need a ret code for EOF to handle better
+            # -2 is the current ret when EOF is hit, but it's not specific
             if ret != 0:
+                print("slow5_get_next reached end of record (-2):", ret, file=sys.stderr)
                 break
             row = {}
 
@@ -84,12 +100,21 @@ cdef class slow5py:
             row['range'] = self.read.range
             row['sampling_rate'] = self.read.sampling_rate
             row['len_raw_signal'] = self.read.len_raw_signal
-            row['signal'] = []
-            for i in range(self.read.len_raw_signal):
-                row['signal'].append(self.read.raw_signal[i])
+            row['signal'] = [self.rec.raw_signal[i] for i in range(self.rec.len_raw_signal)]
+            # for i in range(self.read.len_raw_signal):
+            #     row['signal'].append(self.read.raw_signal[i])
 
             # if pA=True, convert signal to pA
             if pA:
                 # call some conversion function
                 pass
             yield row
+
+
+    def yield_read_list(self, read_list, pA=False):
+        '''
+        returns generator for sequential reading of slow5 file
+        if pa=True, do pA conversion of signal here.
+        '''
+        for r in read_list:
+            yield self._get_read(r, pA=pA)
