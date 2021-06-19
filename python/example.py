781fe5dd60519d9fdab2d1a4617b82ade50fcf85
Psy-Fer <fergo1988@gmail.com>
2021-06-19 16:34:56 +1000

    not working, but fixing head

--- python/example.py
+++ python/example.py
@@ -9,15 +9,18 @@ print("|==============================================|")
 
 # open file and create object
 start_time = time.time()
-s5p = pyslow5.slow5py('examples/example.slow5','r', DEBUG=1)
+# s5p = pyslow5.slow5py('examples/example.slow5','r', DEBUG=1)
+s5p = pyslow5.slow5py('test/data/exp/aux_array/exp_lossless.slow5','r', DEBUG=1)
 ttime = round(time.time() - start_time, 4)
 print("slow5 file opened and object created in: {} seconds".format(ttime))
 print("==============================================")
 
-# Get data for ONE individual read, random access
-print("get_read check, r1")
+# # Get data for ONE individual read, random access
+# print("get_read check, r1")
+print("get_read check, a649a4ae-c43d-492a-b6a1-a5b8b8076be4")
 start_time = time.time()
-read = s5p.get_read("r1")
+# read = s5p.get_read("r1")
+read = s5p.get_read("a649a4ae-c43d-492a-b6a1-a5b8b8076be4")
 ttime = round(time.time() - start_time, 4)
 print("get_read in: {} seconds".format(ttime))
 
@@ -31,41 +34,43 @@ print("sampling_rate:", read['sampling_rate'])
 print("len_raw_signal:", read['len_raw_signal'])
 print("signal:", read['signal'][:10])
 print("pylen of signal:", len(read['signal']))
-
-print("==============================================")
-
-# Get data for ANOTHER ONE individual read, random access, check memory
-print("get_read check, r4, converte to pA")
-start_time = time.time()
-read = s5p.get_read("r4", pA=True)
-ttime = round(time.time() - start_time, 4)
-print("get_read in: {} seconds".format(ttime))
-
-# print all fields
-print("read_id:", read['read_id'])
-print("read_group:", read['read_group'])
-print("digitisation:", read['digitisation'])
-print("offset:", read['offset'])
-print("range:", read['range'])
-print("sampling_rate:", read['sampling_rate'])
-print("len_raw_signal:", read['len_raw_signal'])
-print("signal:", read['signal'][:10])
-print("pylen of signal:", len(read['signal']))
-print("signal value type:", type(read['signal'][0]))
-
-print("==============================================")
-
-# get yield object and iterate through reads sequentially
-print("seq_reads check, all reads")
-start_time = time.time()
-print("seq_reads readIDs:")
-reads = s5p.seq_reads()
-print("type check reads:", type(reads))
-for read in reads:
-    print(read['read_id'])
-
-ttime = round(time.time() - start_time, 4)
-print("seq_reads in: {} seconds".format(ttime))
+print("read:")
+print(read)
+#
+# print("==============================================")
+#
+# # Get data for ANOTHER ONE individual read, random access, check memory
+# print("get_read check, r4, converte to pA")
+# start_time = time.time()
+# read = s5p.get_read("r4", pA=True)
+# ttime = round(time.time() - start_time, 4)
+# print("get_read in: {} seconds".format(ttime))
+#
+# # print all fields
+# print("read_id:", read['read_id'])
+# print("read_group:", read['read_group'])
+# print("digitisation:", read['digitisation'])
+# print("offset:", read['offset'])
+# print("range:", read['range'])
+# print("sampling_rate:", read['sampling_rate'])
+# print("len_raw_signal:", read['len_raw_signal'])
+# print("signal:", read['signal'][:10])
+# print("pylen of signal:", len(read['signal']))
+# print("signal value type:", type(read['signal'][0]))
+#
+# print("==============================================")
+#
+# # get yield object and iterate through reads sequentially
+# print("seq_reads check, all reads")
+# start_time = time.time()
+# print("seq_reads readIDs:")
+# reads = s5p.seq_reads()
+# print("type check reads:", type(reads))
+# for read in reads:
+#     print(read['read_id'])
+#
+# ttime = round(time.time() - start_time, 4)
+# print("seq_reads in: {} seconds".format(ttime))
 
 print("==============================================")
 
@@ -106,6 +111,21 @@ print(f"heatsink_temp: {val}")
 for attr in names:
     val = s5p.get_header_value(attr)
     print(f"{attr}: {val}")
+
+print("==============================================")
+# get aux names
+print("Get aux names")
+aux_names = s5p.get_aux_names()
+print("aux names:")
+print(aux_names)
+
+print("==============================================")
+# get aux values
+
+print("Get aux types")
+aux_types = s5p.get_aux_types()
+print("aux types:")
+print(aux_types)
 
 
 print("==============================================")
