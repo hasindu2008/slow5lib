6bdca6f3cfddaf063efcc7053f9a0d06f187d910
Hiruna Samarakoon <hiruna72@gmail.com>
2021-07-21 16:26:56 +0530

    Update build.sh

--- examples/build.sh
+++ examples/build.sh
@@ -8,4 +8,4 @@ gcc -Wall -O2 -I include/ examples/random_read.c lib/libslow5.a  -o examples/ran
 gcc -Wall -O2 -I include/ examples/get_aux_field.c lib/libslow5.a  -o examples/get_aux_field -lm -lz
 gcc -Wall -O2 -I include/ examples/get_hdr_attribute.c lib/libslow5.a  -o examples/get_hdr_attribute -lm -lz
 gcc -Wall -O2 -I include/ examples/pthread_get_reads.c lib/libslow5.a  -o examples/pthread_get_reads -lm -lz -lpthread
-gcc -Wall -O2 -I include/ examples/openmp_get_reads.c lib/libslow5.a  -o examples/openmp_get_reads -lm -lz -fopenmp
+#gcc -Wall -O2 -I include/ examples/openmp_get_reads.c lib/libslow5.a  -o examples/openmp_get_reads -lm -lz -fopenmp
