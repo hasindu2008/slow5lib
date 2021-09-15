a463311d82b306b96026ebd5c1f60e1a4d07b947
Psy-Fer <fergo1988@gmail.com>
2021-09-15 18:50:31 +1000

    fix building for pypi and including svb-zz

--- setup.py
+++ setup.py
@@ -1,14 +1,46 @@
 try:
     from setuptools import setup, Extension
+    from setuptools.command.install import install
+
 except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
-import numpy as np
+import subprocess
+import sys
 
-cmdclass={}
-from Cython.Build import build_ext
-cmdclass['build_ext'] = build_ext
+include_dirs = []
+# import numpy as np
+
+# creat dummy closures for metadata reading on first parse of setup.py
+# that way it picks up the requirements and installs them, then can use them
+# for the install.
+try:
+    import numpy as np
+    include_dirs = ['include/', np.get_include(), 'thirdparty/streamvbyte/include']
+except ImportError:
+    include_dirs = ['include/', 'thirdparty/streamvbyte/include']
+    def np(*args, ** kwargs ):
+        import numpy as np
+        return np(*args, ** kwargs)
+
+try:
+    from Cython.Build import build_ext
+except ImportError:
+    def build_ext(*args, ** kwargs ):
+        from Cython.Build import build_ext
+        return build_ext(*args, ** kwargs)
+
+# from Cython.Build import build_ext
+
+class CustomInstall(install):
+    def run(self):
+        command = ["make"]
+        print("running make command!!!!")
+        if subprocess.call(command) != 0:
+            sys.exit(-1)
+        install.run(self)
+
 
 #adapted from https://github.com/lh3/minimap2/blob/master/setup.py
 
@@ -19,7 +51,7 @@ depends=['python/pyslow5.pxd', 'python/pyslow5.h',
             'src/slow5_extra.h', 'src/slow5_idx.h', 'src/slow5_misc.h', 'src/klib/ksort.h' ]
 extra_compile_args = ['-g', '-Wall', '-O2', '-std=c99']
 
-include_dirs = ['include/', np.get_include(), 'thirdparty/streamvbyte/include']
+# include_dirs = ['include/', np.get_include(), 'thirdparty/streamvbyte/include']
 libraries = ['m', 'z', 'streamvbyte']
 library_dirs = ['.','thirdparty/streamvbyte/']
 
@@ -31,6 +63,7 @@ extensions = [Extension('pyslow5',
                   include_dirs = include_dirs,
                   library_dirs = library_dirs,
                   language = 'c' )]
+
 
 setup(
     name = 'pyslow5',
@@ -45,10 +78,17 @@ setup(
     license = 'MIT',
     keywords = ['nanopore','slow5','signal'],
     ext_modules=extensions,
-    # requires=['numpy (>=1.3.0)'],
-    #packages=packages,
-    cmdclass=cmdclass,
+    cmdclass= {'install': CustomInstall,
+               'build_ext': build_ext},
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: C',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'],
     python_requires='>=3.4.3',
     install_requires=["numpy"],
-    setup_requires=["cython"]
+    setup_requires=["cython", "numpy"]
     )
