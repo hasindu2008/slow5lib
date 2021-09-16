20e890d3772fb39ac1167bbd3e2d028d1a7cf34f
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-09-16 17:57:00 +1000

    changed the version to dirty

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def readme():
 
 setup(
     name = 'pyslow5',
-    version='0.3.0',
+    version='0.3.0a1',
     url = 'https://github.com/hasindu2008/slow5lib',
     description='slow5lib python bindings',
     long_description=readme(),
