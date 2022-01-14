1230434b8618206c217c957b22f5d7c079b3448c
hiruna72 <hiruna72@gmail.com>
2022-01-15 10:51:56 +1100

    more descriptive logging

--- python/pyslow5.pyx
+++ python/pyslow5.pyx
@@ -60,8 +60,8 @@ cdef class Open:
     cdef float *e20
     cdef double *e21
     cdef char *e22 # meant to be string, not sure about this
-    cdef pyslow5.float time_slow5_get_next
-    cdef pyslow5.float time_yield_a_read
+    cdef pyslow5.float total_time_slow5_get_next
+    cdef pyslow5.float total_time_yield_a_read
 
     def __cinit__(self, pathname, mode, DEBUG=0):
         # Set to default NULL type
@@ -100,8 +100,8 @@ cdef class Open:
         self.e20 = NULL
         self.e21 = NULL
         self.e22 = NULL
-        self.time_slow5_get_next = 0.0
-        self.time_yield_a_read = 0.0
+        self.total_time_slow5_get_next = 0.0
+        self.total_time_yield_a_read = 0.0
 
         # sets up logging level/verbosity
         self.V = DEBUG
@@ -155,8 +155,8 @@ cdef class Open:
         if self.s5 is not NULL:
             pyslow5.slow5_close(self.s5)
         self.logger.warning("pathname: {}".format(self.p))
-        self.logger.warning("time_slow5_get_next: {} seconds".format(self.time_slow5_get_next))
-        self.logger.warning("time_yield_a_read: {} seconds".format(self.time_yield_a_read))
+        self.logger.warning("total_time_slow5_get_next: {} seconds".format(self.total_time_slow5_get_next))
+        self.logger.warning("total_time_yield_a_read: {} seconds".format(self.total_time_yield_a_read))
 
 
     def _convert_to_pA(self, d):
@@ -705,7 +705,7 @@ cdef class Open:
         while ret >= 0:
             start_slow5_get_next = time.time()
             ret = slow5_get_next(&self.read, self.s5)
-            self.time_slow5_get_next = self.time_slow5_get_next + (time.time() - start_slow5_get_next)
+            self.total_time_slow5_get_next = self.total_time_slow5_get_next + (time.time() - start_slow5_get_next)
 
             self.logger.debug("slow5_get_next return: {}".format(ret))
             # check for EOF or other errors
@@ -784,7 +784,7 @@ cdef class Open:
             # if aux data update main dic
             if aux_dic:
                 row.update(aux_dic)
-            self.time_yield_a_read = self.time_yield_a_read + (time.time() - start_slow5_get_next)
+            self.total_time_yield_a_read = self.total_time_yield_a_read + (time.time() - start_slow5_get_next)
             yield row
 
 
