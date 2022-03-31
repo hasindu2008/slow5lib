a7eec6827389179551f04968c323f53c31e94ced
Psy-Fer <fergo1988@gmail.com>
2022-03-31 18:14:46 +1100

    add multithreading headers

--- python/slow5threads.h
+++ python/slow5threads.h
@@ -5,7 +5,9 @@
 #ifndef SLOW5THREADS_H
 #define SLOW5THREADS_H
 
+int slow5_get_batch(slow5_rec_t ***read, slow5_file_t *s5p, char **rid, int num_rid, int num_threads);
 int slow5_get_next_batch(slow5_rec_t ***read, slow5_file_t *s5p, int batch_size, int num_threads);
+int slow5_write_batch(slow5_rec_t **read, slow5_file_t *s5p, int batch_size, int num_threads);
 void slow5_free_batch(slow5_rec_t ***read, int num_rec);
 
 #endif
