f6bcd8f34001ced19f98c5006ab337514af64efc
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2022-06-28 12:36:56 +1000

    update version to 0.5.1

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ def readme():
 
 setup(
     name = 'pyslow5',
-    version='0.5.0-dirty',
+    version='0.5.1',
     url = 'https://github.com/hasindu2008/slow5lib',
     description='slow5lib python bindings',
     long_description=readme(),
