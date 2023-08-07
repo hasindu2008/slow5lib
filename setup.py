ce33ea705b1347e0ebed6e63072e527954a170f8
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-08-07 12:53:10 +1000

    -dev in setup.py

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ def readme():
 
 setup(
     name = 'pyslow5',
-    version='1.0.0-dirty',
+    version='1.0.0-dev',
     url = 'https://github.com/hasindu2008/slow5lib',
     description='slow5lib python bindings',
     long_description=readme(),
