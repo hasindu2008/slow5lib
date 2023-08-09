1150ba21c8b84ea714b60edade81e502b74a7884
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-08-09 17:32:31 +1000

    bump pyslow5 to 1.0.1-beta

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ def readme():
 
 setup(
     name = 'pyslow5',
-    version='1.0.0-dev',
+    version='1.0.1-beta',
     url = 'https://github.com/hasindu2008/slow5lib',
     description='slow5lib python bindings',
     long_description=readme(),
